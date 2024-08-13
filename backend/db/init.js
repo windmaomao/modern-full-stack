ec6b87604e6cf5dc546820639a6e14d5923a38db
Fang Jin <windmaomao@gmail.com>
2024-08-13 14:41:39 -0400

    feat: move db andservice under backend

--- backend/db/init.js
+++ backend/db/init.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export function initDatabase() {
-  const DATABASE_URL = 'mongodb://localhost:27017/test'
+  const DATABASE_URL = process.env.DATABASE_URL
 
   mongoose.connection.on('open', () => {
     console.info('successfully connected to database:', DATABASE_URL)
