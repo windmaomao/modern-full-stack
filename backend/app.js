fafc5214e9af194ba60cdf09bc53c98b1d422bdf
Fang Jin <windmaomao@gmail.com>
2024-08-14 12:28:45 -0400

    feat: add cors support

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
 import { postsRoutes } from './routes/posts.js'
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 postsRoutes(app)
 
 app.get('/', (req, res) => {
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,9 @@ import {
   listPostsByAuthor,
   listPostsByTag,
   getPostById,
+  createPost,
+  updatePost,
+  deletePost,
 } from '../services/posts.js'
 
 export function postsRoutes(app) {
@@ -36,6 +39,37 @@ export function postsRoutes(app) {
       return res.json(post)
     } catch (err) {
       console.error('error getting post', err)
+      return res.status(500).end()
+    }
+  })
+
+  app.post('/api/v1/posts', async (req, res) => {
+    try {
+      const post = await createPost(req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('error creating post', err)
+      return res.status(500).end()
+    }
+  })
+
+  app.patch('/api/v1/posts/:id', async (req, res) => {
+    try {
+      const post = await updatePost(req.params.id, req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('error updating post', err)
+      return res.status(500).end()
+    }
+  })
+
+  app.delete('/api/v1/posts/:id', async (req, res) => {
+    try {
+      const { deletedCount } = await deletePost(req.params.id)
+      if (deletedCount === 0) return res.sendStatus(404)
+      return res.status(204).end()
+    } catch (err) {
+      console.error('error deleting post', err)
       return res.status(500).end()
     }
   })
