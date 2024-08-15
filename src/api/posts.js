65db5c6cef391c3f65b220d644f3627b4180e986
Fang Jin <windmaomao@gmail.com>
2024-08-15 13:25:22 -0400

    feat: use mutation to create post

--- src/api/posts.js
+++ src/api/posts.js
@@ -4,3 +4,12 @@ export async function getPosts(queryParams) {
   const res = await fetch(url + '/posts?' + new URLSearchParams(queryParams))
   return await res.json()
 }
+
+export const createPost = async (post) => {
+  const res = await fetch(url + '/posts', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(post),
+  })
+  return await res.json()
+}
