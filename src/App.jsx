5f8d994441445bba27117afc3c13196d844e4c87
Fang Jin <windmaomao@gmail.com>
2024-08-15 12:13:53 -0400

    feat: add posts component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { PostList } from './components/PostList'
+import { CreatePost } from './components/CreatePost'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
+
+const posts = [
+  {
+    title: 'Full-Stack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'Daniel Bugl',
+  },
+  { title: 'Hello React!' },
+]
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,9 +34,17 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div style={{ padding: 8 }}>
+          <CreatePost />
+          <br />
+          <hr />
+          Filter by:
+          <PostFilter field='author' />
+          <br />
+          <PostSorting fields={['createdAt', 'updatedAt']} />
+          <hr />
+          <PostList posts={posts} />
+        </div>
       </div>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
---
+++ src/components/PostFilter.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'
+
+export function PostFilter({ field }) {
+  return (
+    <div>
+      <label htmlFor={`filter-${field}`}>{field}: </label>
+      <input type='text' name={`filter-${field}`} id={`filter-${field}`} />
+    </div>
+  )
+}
+PostFilter.propTypes = {
+  field: PropTypes.string.isRequired,
+}
